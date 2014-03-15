527250c39d775ccd0742312740d14ed4fb8ac684
Sekerak <sekys.sekys@gmail.com>
2014-03-16 00:15:19 +0100

    Logovanie sprav sa zapisuje do suboru.
    Overena funkcionalita kamery.
    
    Change-Id: I3aeb42b40bed5ab93e090e82e3f72312170bc98f
    
    Former-commit-id: 3db60c15c9447265833722ee06daff3f1155fc3c

--- com.carlos.architecture/configuration/class.Configuration.cpp
+++ com.carlos.architecture/configuration/class.Configuration.cpp
@@ -20,6 +20,17 @@ float Configuration::str_to_float(const std::string &in)
 	return f;
 }
 
+float Configuration::int_to_float(const std::string &in)
+{
+	std::istringstream  o(in); 
+	int f; 
+	if (!(o >> f)) {
+		throw new std::exception("str_to_float");
+	}
+	o >> f; 
+	return f;
+}
+
 const XMLElement* Configuration::getRoot() {
 	return doc.FirstChildElement( "configuration" );
 }
@@ -30,6 +41,10 @@ const XMLElement* Configuration::getConfig(const char *name) {
 
 const float Configuration::getConfigf(const char *name) {
 	return str_to_float( getConfig(name)->GetText() );
+}
+
+const int Configuration::getConfigi(const char *name) {
+	return int_to_float( getConfig(name)->GetText() );
 }
 
 const char* Configuration::getTitle() {
