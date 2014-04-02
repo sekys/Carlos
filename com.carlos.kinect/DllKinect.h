ee2c944ff3390ef24029e1c4e2fd1d1edac79a34
xmercz <merczj@gmail.com>
2014-04-02 15:02:56 +0200

    prerobenie Kinect modulu podla examplu miesto wrappera, z dovodu deadlocku vo wrappery
    Change-Id: If5d65b76f9930e2d9366989bc2f20a55e0a19da6
    
    Former-commit-id: 90b5c76df58d867383909fc1d32134b19a62a066

--- com.carlos.kinect/DllKinect.h
+++ com.carlos.kinect/DllKinect.h
@@ -2,12 +2,59 @@
 #define NOMINMAX
 #include "../com.carlos.architecture/modules/class.ModulKinect.hpp"
 #include "../libs/include/freenect/libfreenect.hpp"
-#include "MyFreenectDevice.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "libfreenect.h"
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <opencv/cv.h>
+#include <opencv/cxcore.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <fstream>
+
+#include <pthread.h>
+
+#include <math.h>
+
+using namespace cv;
+using namespace std;
 
 using namespace Architecture;
 
 class DllKinect : public ModulKinect {
 public:
+
+	int die;
+
+
+Mat M;
+
+pthread_mutex_t gl_backbuf_mutex;
+
+uint8_t *rgb_back, *rgb_mid;
+
+Mat depthMat;
+Mat depthf;
+Mat rgbMat;
+Mat grayMat;
+
+CascadeClassifier face_cascade;
+
+freenect_context *f_ctx;
+freenect_device *f_dev;
+int freenect_angle;
+int freenect_led;
+
+bool m_new_rgb_frame;
+bool m_new_depth_frame;
+
+uint16_t *depth;
+
 	virtual Point3f getAktualnaRotaciaHlavy();
 	virtual void init();
 
@@ -18,6 +65,12 @@ public:
 	*	Tieto body potom sú spracované do matíc a je z nich vypoèítaná transformaèná matica.
 	*/
 
-	virtual void spustiKalibraciu();
+	virtual void NacitajMaticu();
+	virtual void freenect_threadfunc();
+	virtual Vec3f DepthToWorld(int x, int y, int depthValue);
+	virtual void getDepth();
+	virtual void getRGB();
+	virtual void rgb_cb(freenect_device *dev, void *rgb, uint32_t timestamp);
+	virtual void depth_cb(freenect_device *dev, void *v_depth, uint32_t timestamp);
 };
 
